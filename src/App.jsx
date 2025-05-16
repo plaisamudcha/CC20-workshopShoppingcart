57eb57274ece66d86d4d9e48c24ec7f803d73fbd
plaisamudcha <plai_samudcha@hotmail.com>
2025-05-16 09:20:40 +0700

    starter with tailwing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect,useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl'>Codecamp20</h1>
+      <button className='btn'>OK</button>
+    </div>
+  )
+}
+
+export default App
