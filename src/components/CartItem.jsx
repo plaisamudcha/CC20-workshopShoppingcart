e9fdc61073c06cc744be347367d342943307f4ad
plaisamudcha <plai_samudcha@hotmail.com>
2025-05-16 16:51:02 +0700

    finished cal before change UI

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,12 +2,12 @@ export default function CartItem (props) {
   const {item:{id,title,price,quantity},decQuantity,addToCart} = props
   return (
     <div className="flex justify-between p-2 bg-accent-content rounded-md items-baseline">
-      <p className="">{title.split(' ')[0]}</p>
+      <p className="min-w-2/5">{title.split(' ')[0]}</p>
       <div className="flex gap-1.5">
         <button onClick={() => {decQuantity(id)}} className="btn">-</button>
         <button onClick={() => {addToCart(id)}} className="btn">+</button>
       </div>
-      <p>{quantity}*{price}</p>
+      <p className="min-w-2/5 text-end">{quantity}*${price}</p>
     </div>
   )
 }
--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
 import CartItem from "./CartItem"
 
 export default function CartSummary (props) {
   const {carts,decQuantity,addToCart} = props
+  const total = carts.reduce((prev,curr) => prev+=curr.price*curr.quantity,0)
+  const tax = total*0.07
+  const sum = total + tax
   return (
     <div className=" p-4 w-1/3 flex flex-col gap-4">
       <h2 className="border border-gray-500 text-2xl rounded p-2 w-auto inline">Cart Item :</h2>
@@ -9,6 +13,27 @@ export default function CartSummary (props) {
       <div className="flex flex-col gap-2.5">
         {carts.map (el => <CartItem key={el.id} item={el} decQuantity = {decQuantity} addToCart = {addToCart} />)}
       </div>
+      <hr />
+      {carts.length > 0 && (
+        <div className="flex flex-col gap-2.5">
+          <div className="flex justify-between">
+            <p>Item price</p>
+            <p>${total.toFixed(2)}</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Tax</p>
+            <p>${tax.toFixed(2)}</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Shipping</p>
+            <p>$0</p>
+          </div>
+          <div className="flex justify-between">
+            <p>Total</p>
+            <p className="underline decoration-double">${sum.toFixed(2)}</p>
+          </div>
+      </div>
+      )}
     </div>
   )
 }
